fb99ba2736e78ccb287718f3cbd80267989078ac
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-24 04:37:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if number % 5 == 0 && number % 3 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
+  number.times do |count|
+    count += 1
+    if count % 5 == 0 && count % 3 == 0
+      puts "FizzBuzz"
+    elsif count % 3 == 0
+      puts "Fizz"
+    elsif count % 5 == 0
+      puts "Buzz"
+    else
+      puts count
+    end
   end
 end
+
+puts "Tell me"
+number = gets.chomp.to_i
+fizzbuzz(number)
